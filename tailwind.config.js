9f5ffd5ed0cea4f6bdf5808e13dc922c56d17c5e
Aaron Myatt <aaronmyatt@gmail.com>
2023-05-23 01:35:17 +0800

    add tailwind + daisy

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/**/*.html",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+}
+
