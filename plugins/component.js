8a2519503e4a09ab81d49b33257eace5f2f72448
Aaron Myatt <aaronmyatt@gmail.com>
2023-05-25 00:23:32 +0800

    improve prop handling, fix slot binding

--- plugins/component.js
+++ plugins/component.js
@@ -1,7 +1,5 @@
 // Define a new Alpine.js plugin
 export default function registerWebComponents(Alpine) {
-    // Define the prefix for property attributes
-    const PROPERTY_ATTRIBUTE_PREFIX = 'data-';
     const ALPINE_ATTRIBUTE_PREFIX = 'x-';
     // Find all <template> tags with the `x-component` directive
     document.querySelectorAll('template[x-component]').forEach((template) => {
@@ -9,21 +7,20 @@ export default function registerWebComponents(Alpine) {
         const componentName = template.getAttribute('x-component');
 
         // Get the property names and default values from the <template> tag
-        const properties = {};
-        const alpineDirectives = {};
-        Array.from(template.attributes).forEach((attribute) => {
-            if (attribute.name.startsWith(PROPERTY_ATTRIBUTE_PREFIX)) {
-                const propertyName = attribute.name.replace(
-                    PROPERTY_ATTRIBUTE_PREFIX,
-                    ''
-                );
-                properties[propertyName] = attribute.value;
-            }
+        const properties = Object.assign({}, template.dataset);
+        const alpineDirectives = template.getAttributeNames()
+            .filter((attributeName) => {
+                return attributeName.startsWith(ALPINE_ATTRIBUTE_PREFIX) && attributeName !== "x-component";
+            })
+            .map((attributeName) => {
+                return {
+                    [attributeName]: template.getAttribute(attributeName)
+                }
+            })
+            .reduce((acc, curr) => {
+                return { ...acc, ...curr };
+            }, {});
 
-            if (attribute.name.startsWith(ALPINE_ATTRIBUTE_PREFIX)) {
-                alpineDirectives[attribute.name] = attribute.value;
-            }
-        });
 
         // Define a new WebComponent class
         class CustomElement extends HTMLElement {
@@ -35,19 +32,34 @@ export default function registerWebComponents(Alpine) {
                 // Wrap the <template> tag in a <div> tag with the `x-data` attribute
                 this.alpineWrapper = document.createElement('div');
                 properties.componentName = componentName;
-                this.alpineWrapper.setAttribute(
-                    'x-data',
-                    JSON.stringify(properties)
-                );
+                properties.slots = {
+                    default: {}
+                    // convert componentName like card-button, to function name like cardButton
+                }
+
+                if ('x-data' in alpineDirectives) {
+                    // skip!
+                } else {
+                    // const alpineDataName = componentName.split("-").map((word, index) => {
+                    //     if (index === 0) {
+                    //         return word;
+                    //     }
+                    //     return word.charAt(0).toUpperCase() + word.slice(1);
+                    // }).join("");
+                    // const data = structuredClone(properties);
+                    // Alpine.data(alpineDataName, _ => data)
+                    this.alpineWrapper.setAttribute('x-data', JSON.stringify(properties));
+                }
+
                 Object.keys(alpineDirectives).forEach((directive) => {
                     this.alpineWrapper.setAttribute(directive, alpineDirectives[directive]);
                 });
-                
+
                 // Define getters and setters for each property
                 Object.keys(properties).forEach((propertyName) => {
                     Object.defineProperty(this, propertyName, {
                         get() {
-                            return Alpine.$data(this.shadowRoot)[propertyName]
+                            return Alpine.$data(this.shadowRoot.firstChild)[propertyName]
                         },
                         set(value) {
                             this.shadowRoot && (Alpine.$data(this.shadowRoot.firstChild)[propertyName] = value);
@@ -57,31 +69,17 @@ export default function registerWebComponents(Alpine) {
             }
 
             async connectedCallback() {
-                const componentTemplate = await this.cloneOrFetchTemplate();
-                // Get the contents of the <template> tag and attach it to the shadow DOM
-                this.alpineWrapper.appendChild(componentTemplate);
-                
                 this.attachShadow({ mode: 'open' });
+                // Get the contents of the <template> tag and attach it to the shadow DOM
+                const componentTemplate = await this.cloneOrFetchTemplate();
+                this.alpineWrapper.appendChild(componentTemplate);
                 this.shadowRoot.appendChild(this.alpineWrapper);
-
-                const style = document.createElement("style");
-                style.textContent = `@import url(${document.styleSheets[0].href})`
-
-                this.shadowRoot.appendChild(style);
+                this.shareStylesWithDocument();
 
                 Alpine.initTree(this.shadowRoot.firstChild);
 
-                Object.keys(properties).forEach((propertyName) => {
-                    if(this.hasAttribute(propertyName)) {
-                        this[propertyName] = this.getAttribute(propertyName);
-                    }
-                });
-
-                // let slots = this.querySelectorAll("slot").forEach((slot) => {
-                //     slot.addEventListener("slotchange", (e) => {
-                //         // console.log(slot.assignedElements());
-                //     });
-                // })
+                this.registerProps()
+                this.registerSlots()
             }
 
             static get observedAttributes() {
@@ -106,7 +104,7 @@ export default function registerWebComponents(Alpine) {
                 // otherwise fetch it from the public folder
                 if (hasNonSlotChildren) {
                     return template.content.cloneNode(true);
-                }else {
+                } else {
                     return fetch(`${componentName}.html`)
                         .then((response) => response.text())
                         .then((html) => {
@@ -116,6 +114,29 @@ export default function registerWebComponents(Alpine) {
                             return template.content.cloneNode(true);
                         });
                 }
+            }
+
+            shareStylesWithDocument() {
+                const style = document.createElement("style");
+                style.textContent = `@import url(${document.styleSheets[0].href})`
+                this.shadowRoot.appendChild(style);
+            }
+
+            registerProps() {
+                Object.keys(properties).forEach((propertyName) => {
+                    if (this.hasAttribute(propertyName)) {
+                        this[propertyName] = this.getAttribute(propertyName);
+                    }
+                });
+            }
+
+            registerSlots() {
+                this.shadowRoot.querySelectorAll("slot").forEach((slot) => {
+                    slot.addEventListener("slotchange", (e) => {
+                        const slotChild = slot.assignedElements().slice(0, 1).pop();
+                        this.slots[slot.name || 'default'] = slotChild;
+                    });
+                })
             }
         }
 
