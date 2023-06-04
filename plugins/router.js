85865361745b080627fd0ee3739e53dde9a9feeb
Aaron Myatt <aaronmyatt@gmail.com>
2023-06-04 10:53:47 +0800

    simplify directives

--- plugins/router.js
+++ plugins/router.js
@@ -51,131 +51,31 @@ export default function (Alpine) {
     Alpine.router = Router;
     Alpine.views = Views;
 
-    Alpine.directive('route', (el, { expression }, { evaluateLater, effect }) => {
+    Alpine.directive('route', (el, {expression, modifiers}, {evaluateLater, effect}) => {
         Router.routes.push(el);
 
         const getRoute = expression.startsWith('/') ? (fn) => fn(expression) : evaluateLater(expression);
-        effect(() => {
+        el.addEventListener('click', (e) => {
             getRoute((route) => {
-                el.addEventListener('click', (event) => {
-
-                    target = el.getAttribute('x-target') || Alpine.defaultTarget
-                    if (linkIsInternal(route)) {
-                        event && event.preventDefault()
-                        Router._rawPath = route
-                        // separate path from query
-                        const [path, query] = route.split('?')
-                        const realPath = path
-                            .split('/')
-                            .filter((key) => key !== '')
-                            .reduce((pathToBe, pathPart) => {
-                                // extract key from key if key is "key:value"
-                                if (pathPart.includes(':')) {
-                                    const [paramKey, paramValue] = pathPart.split(':')
-                                    Router.params[paramKey] = paramValue
-                                    return pathToBe + paramValue + '/'
-                                } else {
-                                    return pathToBe + pathPart + '/'
-                                }
-                            }, '/')
-                        Router.push(realPath, query && getQueryParams(`?${query}`))
-                    }
-                })
+                target = el.getAttribute('x-target') || Alpine.defaultTarget
+                if (linkIsInternal(route)) {
+                    e && e.preventDefault()
+                    Router.push(route)
+                }
             })
         })
-
     })
 
     Alpine.magic('route', (el, {Alpine}) => expression => {
         target = el.getAttribute('x-target') || Alpine.defaultTarget
-            if (linkIsInternal(expression)) {
-                Router._rawPath = expression
-                // separate path from query
-                const [path, query] = expression.split('?')
-                const realPath = path
-                    .split('/')
-                    .filter((key) => key !== '')
-                    .reduce((pathToBe, pathPart) => {
-                        // extract key from key if key is "key:value"
-                        if (pathPart.includes(':')) {
-                            const [paramKey, paramValue] = pathPart.split(':')
-                            Router.params[paramKey] = paramValue
-                            return pathToBe + paramValue + '/'
-                        } else {
-                            return pathToBe + pathPart + '/'
-                        }
-                    }, '/')
-                Router.push(realPath, query && getQueryParams(`?${query}`))
-            }
+        if (linkIsInternal(expression)) {
+            Router.push(expression.pathname, query && getQueryParams(`?${query}`))
+        }
     })
 
-    Alpine.directive('view', (el, { expression }, { effect }) => {
-
-        effect(() => {
-            if (el.hasAttribute('x-target'))
-                target = el.getAttribute('x-target')
-
-            let view = Router._rawPath
-                .split('/')
-                .filter((key) => key !== '')
-                .reduce((pathToBe, pathPart, index, array) => {
-                    // extract key from key if key is "key:value"
-                    if (pathPart.includes(':')) {
-                        const [paramKey, _] = pathPart.split(':')
-                        pathPart = paramKey
-                    }
-                    if (index === array.length - 1) {
-                        return pathToBe + pathPart
-                    }
-                    return pathToBe + pathPart + '/'
-                }, '/')
-
-            if (view === expression) {
-                // if element innerHTML empty, fetch template from public folder
-                if (el.innerHTML.trim() === '') {
-                    fetch(`${expression}.html`)
-                        .then((response) => response.text())
-                        .then((html) => {
-                            el.innerHTML = html
-                        })
-                        .then(() => {
-                            renderView(target, el.innerHTML);
-                        })
-                } else {
-                    renderView(target, el.innerHTML);
-                }
-            }
-        })
-
-        function renderView(target, html){
-            const notInTheShadowDom = document.querySelector(target)
-            if(notInTheShadowDom){
-                notInTheShadowDom.innerHTML = html;
-                Alpine.initTree(notInTheShadowDom);
-            }
-            else
-                window.components && window.components.map(component => {
-                    const el = component.shadowRoot.querySelector(target)
-                    if(el){
-                        component.shadowRoot.querySelector(target).innerHTML = html
-                        Alpine.initTree(el);
-                    }
-                })
-        }
-
-        // split route into object keys
-        // if route is /foo/bar/baz
-        // then we want to create an object like this:
-        // {
-        //   foo: {
-        //     bar: {
-        //       baz: el
-        //     }
-        //   }
-        // }
-        const keys = expression.split('/')
-            .filter((key) => key !== '')
+    Alpine.directive('view', (el, {expression, modifiers}, {effect}) => {
         Views[expression] = el;
+        Views[expression].parts = expression.split('/').filter(part => part !== '');
     })
 
     // ensure all views have been parsed then trigger initial view
