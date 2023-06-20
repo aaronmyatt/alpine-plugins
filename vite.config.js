17c421d64440533952d5b03f09a2bab174c97859
Aaron Myatt <aaronmyatt@gmail.com>
2023-06-20 15:02:28 +0800

    support building libraries and doc site

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,32 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-    build: {
-        minify: false,
-        lib: {
-            // Could also be a dictionary or array of multiple entry points
-            entry: [resolve(__dirname, 'plugins/router.js'), resolve(__dirname, 'plugins/component.js')],
-            // name: 'AlpineRouter',
-            // the proper extensions will be added
-            fileName: (ft, name) => `${name}/index.${ft}.js`
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['alpine'],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    alpine: 'Alpine',
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        base: '/alpine-plugins/',
+        build: {
+            minify: false,
+            lib: env.lib ? {
+                // Could also be a dictionary or array of multiple entry points
+                entry: [resolve(__dirname, 'plugins/router.js'), resolve(__dirname, 'plugins/component.js')],
+                // name: 'AlpineRouter',
+                // the proper extensions will be added
+                fileName: (ft, name) => `${name}/index.${ft}.js`
+            } : false,
+            rollupOptions: {
+                // make sure to externalize deps that shouldn't be bundled
+                // into your library
+                external: ['alpine'],
+                output: {
+                    // Provide global variables to use in the UMD build
+                    // for externalized deps
+                    globals: {
+                        alpine: 'Alpine',
+                    },
                 },
             },
         },
-    },
+    }
 })
